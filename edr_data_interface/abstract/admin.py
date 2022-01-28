79e515efeeeffedb3794b033609f5cb63307456c
DPeterK <peter.killick@informaticslab.co.uk>
2022-01-28 14:40:57 +0000

    Support optional extents

--- edr_data_interface/abstract/admin.py
+++ edr_data_interface/abstract/admin.py
@@ -2,11 +2,49 @@ from .core import Interface
 
 
 class RefreshCollections(Interface):
-    def collection(self, name):
+    def __init__(self, supported_data_queries) -> None:
+        self.supported_data_queries = supported_data_queries
+        self.temporal_extent = False
+        self.vertical_extent = False
+
+        self._get_temporal_extent()
+        self._get_vertical_extent()
+
+    def _get_temporal_extent(self):
+        """
+        Determine if the collection's data has a temporal extent.
+
+        Must set `self.temporal_extent`, and also determine `interval`, `trs` and `name`
+        for the temporal extent JSON response. The `trs` variable must be provided
+        as well-known text (WKT).
+        
+        """
+        self.temporal_extent = False
+
+    def _get_vertical_extent(self):
+        """
+        Determine if the collection's data has a vertical extent.
+
+        Must set `self.vertical_extent`, and also determine `interval`, `vrs` and `name`
+        for the vertical extent JSON response. The `vrs` variable must be provided
+        as well-known text (WKT).
+        
+        """
+        self.vertical_extent = False
+
+    def has_temporal_extent(self):
+        """Define if the collection has a temporal extent."""
+        return self.temporal_extent
+
+    def has_vertical_extent(self):
+        """Define if the collection has a vertical extent."""
+        return self.vertical_extent
+
+    def make_collection(self, name):
         raise NotImplemented
 
-    def collections(self):
+    def make_collections(self):
         raise NotImplemented
 
     def data(self):
-        return self.collections()
+        return self.make_collections()
--- edr_data_interface/concrete/dummy/admin.py
+++ edr_data_interface/concrete/dummy/admin.py
@@ -1,5 +1,4 @@
 from collections import namedtuple
-from dataclasses import dataclass
 from collections import namedtuple
 from typing import Dict, List
 
@@ -7,8 +6,24 @@ from ...abstract.admin import RefreshCollections
 
 
 SAMPLES: Dict = {
-    "one": ["00001", "One", "The first item", "CRS84"],
-    "two": ["00002", "Two", "The second item", "EPSG4326"],
+    "one": [
+        "00001",
+        "One",
+        "The first item",
+        "CRS84",
+        "WGS 1984",
+        [-180, -90, 180, 90],
+        ["Example", "Dummy"],
+    ],
+    "two": [
+        "00002",
+        "Two",
+        "The second item",
+        "EPSG4326",
+        "EPSG4326",
+        [-180, -90, 180, 90],
+        ["Example", "Dummy"],
+    ],
 }
 
 FIELDS: List[str] = [
@@ -16,19 +31,40 @@ FIELDS: List[str] = [
     "name",
     "description",
     "crs",
+    "crs_name",
+    "bbox",
+    "keywords",
 ]
-
-collection = namedtuple("collection", FIELDS)
 
 
 class RefreshCollections(RefreshCollections):
-    def collection(self, name) -> collection:
-        sample = SAMPLES[name]
-        return collection(*sample)
+    def __init__(self, supported_data_queries) -> None:
+        super().__init__(supported_data_queries)
+        self.collection = namedtuple("collection", FIELDS)
 
-    def collections(self) -> List[collection]:
+    def _get_temporal_extent(self):
+        this_extent = True
+        if this_extent:
+            FIELDS.extend(["temporal_interval", "trs", "temporal_name"])
+            SAMPLES["one"].extend(["today", "TIMECRS", "Dummy temporal extent"])
+            SAMPLES["two"].extend(["today/tomorrow", "TIMECRS", "Dummy temporal extent"])
+        self.temporal_extent = this_extent
+
+    def _get_vertical_extent(self):
+        this_extent = True
+        if this_extent:
+            FIELDS.extend(["vertical_interval", "vrs", "vertical_name"])
+            SAMPLES["one"].extend([[2], "VERTCS", "Dummy vertical extent"])
+            SAMPLES["two"].extend([[2, 10], "VERTCS", "Dummy vertical extent"])
+        self.vertical_extent = this_extent
+
+    def make_collection(self, name):
+        sample = SAMPLES[name]
+        return self.collection(*sample)
+
+    def make_collections(self) -> List:
         collections: List = []
         for name in SAMPLES.keys():
-            collection = self.collection(name)
+            collection = self.make_collection(name)
             collections.append(collection)
         return collections
