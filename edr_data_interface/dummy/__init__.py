0b9113693fe2c7ea3981765a568578986860b864
DPeterK <peter.killick@informaticslab.co.uk>
2022-02-24 10:09:29 +0000

    Add items data provider example

--- edr_data_interface/dummy/__init__.py
+++ edr_data_interface/dummy/__init__.py
@@ -1,4 +1,5 @@
 from .admin import RefreshCollections
 from .capabilities import API, Capabilities, Conformance
+from .items import Items
 from .locations import Location, Locations
 from .service import Service
---
+++ edr_data_interface/dummy/items.py
@@ -0,0 +1,43 @@
+from typing import List
+from urllib.parse import urljoin
+
+from edr_server.abstract_data_interface.items import (
+    Feature, FeatureCollection, Items, Parameter
+)
+
+from .locations import Locations
+
+
+class Items(Items):
+    def _get_features(self) -> List[Feature]:
+        # Get features to serve as items.
+        in_features = {}  # In future more EDR contents than locations (e.g. area, cube) might provide features
+        locations_provider = Locations(self.collection_id, self.query_parameters)
+        locations = locations_provider.all_locations()
+        in_features["locations"] = locations
+
+        # Present located features as items.
+        out_features = []
+        for feature_type, features in in_features.items():
+            for feature in features:
+                feature_href = urljoin(self.collection_href, feature_type, feature.id)
+                out_feature = Feature(
+                    feature.id,
+                    feature.geometry_type,
+                    feature.coords,
+                    feature.properties,
+                    feature_href
+                )
+                out_features.append(out_feature)
+        return out_features
+
+    def data(self) -> FeatureCollection:
+        features = self._get_features()
+        n_features = len(features)
+        return FeatureCollection(
+            links=[],
+            number_matched=n_features,
+            number_returned=n_features,
+            timestamp=self._get_timestamp(),
+            items=features
+        )
