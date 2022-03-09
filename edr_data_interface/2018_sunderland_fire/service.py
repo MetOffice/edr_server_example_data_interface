52ba9e5c3c8f8f6d3222360a3a2bd1e6d2cb3d32
Peter Killick <peter.killick@informaticslab.co.uk>
2022-03-09 14:57:23 +0000

    Add sunderland data (#1)
    * Add sunderland fire interface
    
    * Minor changes
    
    * Remove comment lines

---
+++ edr_data_interface/2018_sunderland_fire/service.py
@@ -0,0 +1,13 @@
+from edr_server.abstract_data_interface.service import Service, ServiceData
+
+
+URLS = {
+    "description_url": "https://www.example.org/service/description.html",
+    "license_url": "https://www.example.org/service/licence.html",
+    "terms_url": "https://www.example.org/service/terms-and-conditions.html",
+}
+
+
+class Service(Service):
+    def data(self) -> ServiceData:
+        return ServiceData(**URLS)
