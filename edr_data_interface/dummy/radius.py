f3f5d680b1cf32cebeb29ebeaecce710e1bc3017
DPeterK <peter.killick@informaticslab.co.uk>
2022-03-22 12:03:35 +0000

    Move polygon filter implementations to data interface

--- edr_data_interface/dummy/radius.py
+++ edr_data_interface/dummy/radius.py
@@ -1,6 +1,8 @@
 from itertools import chain
 from typing import List
 
+from shapely.geometry import box
+
 from edr_server.abstract_data_interface.radius import Radius
 from edr_server.abstract_data_interface.locations import Feature
 
@@ -16,3 +18,16 @@ class Radius(Radius):
     def get_collection_bbox(self):
         from .dataset import COLLECTIONS
         return COLLECTIONS[self.collection_id]["bbox"]
+
+    def polygon_filter(self, items: List[Feature]) -> List[Feature]:
+        """
+        Implement a polygon filter that tests for the polygon provided in the
+        query arguments intersecting with the feature's bounding box.
+
+        """
+        result = []
+        for item in items:
+            bbox = box(*item.bbox)
+            if bbox.intersects(self.polygon):
+                result.append(item)
+        return result
