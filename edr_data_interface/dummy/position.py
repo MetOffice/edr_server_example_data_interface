6b877b3cd78a1307c096ee5c77c4c2de96568cbf
DPeterK <peter.killick@informaticslab.co.uk>
2022-03-29 15:09:41 +0100

    Implement position filtering

--- edr_data_interface/dummy/position.py
+++ edr_data_interface/dummy/position.py
@@ -2,24 +2,34 @@ from itertools import chain
 from typing import List, Union
 
 from shapely.geometry import box
+import shapely.wkt as wkt
 
 from edr_server.abstract_data_interface.position import Position
 from edr_server.abstract_data_interface.locations import Feature
 
-from .import filters
 from .items import Items
 
 
 class Position(Position):
-    filter_type = "intersection"
-
-    def _exact_match(self):
+    def _exact_match(self, point, item):
         """Return only items that are at the exact location specified."""
-        pass
+        if item.geometry_type != "point":
+            # We can't exactly point-match if we don't have two points.
+            return self._fuzzy_match(point, item)
+        else:
+            item_point = wkt.loads(f"{item.geometry_type}({float(item.coords[0])} {float(item.coords[1])})")
+            return point == item_point
 
-    def _fuzzy_match(self, fuzz=1):
-        """Return items that are at the location specified, within a small tolerance."""
-        pass
+    def _fuzzy_match(self, point, item):
+        """Return items that are at the location specified, within a small tolerance defined by `self.fuzz`."""
+        xlo, ylo, xhi, yhi = item.bbox
+        fuzzy_geom = box(xlo-self.fuzz, ylo-self.fuzz, xhi+self.fuzz, yhi+self.fuzz)
+        return point.intersects(fuzzy_geom)
+
+    def _matcher(self, point, items):
+        meth = self._exact_match if self.match == "exact" else self._fuzzy_match
+        filtered = filter(lambda item: meth(point, item), items)
+        return list(filtered)
 
     def all_items(self) -> List[Feature]:
         items_provider = Items(self.collection_id, self.query_parameters, "")
@@ -32,5 +42,16 @@ class Position(Position):
         query arguments intersecting with the feature's bounding box.
 
         """
-        # XXX implement!
-        pass
+        self.fuzz = 0.1
+        self.match = "exact"
+
+        result = None
+        geometry = self.query_parameters.get("coords")
+        if geometry.geom_type.lower() == "point":
+            result = self._matcher(geometry, items)
+        else:
+            # We confirmed in `self._check_query_args` that we only had Point/MultiPoint.
+            result = []
+            for point in geometry.geoms:
+                result.extend(self._matcher(point, items))
+        return result
